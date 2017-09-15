cd7e6e71ff35d9a7191fddc7b015df42643f8e0a
inyhow <inyhow@gmail.com>
2017-09-15 14:24:52 +0800

    Signed-off-by: inyhow <inyhow@gmail.com>

---
+++ data/module/b7b83db75309e996d7a4f7a63e4c8076-readme.php
@@ -0,0 +1,2 @@
+<p style='line-height:150%'>1.打包中的文件，默认是覆盖到dede目录的，除了一个plus底下一个view.php<br />
+2.这个插件会修改#@__archives表，直接加了一个view</p>
---
+++ dede/viewarcpwd.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+<title>密码查看文章说明</title>
+<link href='img/base.css' rel='stylesheet' type='text/css' />
+</head>
+<body background='img/allbg.gif' leftmargin='8' topmargin='8'>
+<table width="98%" border="0" align="center" cellpadding="3" cellspacing="1" bgcolor="#D1DDAA">
+
+<tr>
+<td width="10%" style="padding:100px 30px 100px 30px;border:groove 1px #000;">密码查看文章说明：发表文章时，请在查看密码框填写查看密码（只能是纯数字，最多为１１个数字），如果密码不为空白，则文章不能生成HTML静态页，浏览者需要输入正确的密码后才可以查看到文章的内容，否则只能看到文章的标题和文章的简略说明。如果查看密码为空白，则密码查看的功能不起作用，即该功能被禁用，您可以再次编辑文章生成HTML静态页（注意让密码输入框保持为空白状态）。</td>
+</tr>
+</table>
+</body>
+</html>
